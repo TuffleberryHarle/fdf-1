6e55cf6de1e73450d29a6069d9b7dd7eccb4c8b7
tuffleberryharle <ca7chy@ya.ru>
2020-08-01 16:17:50 +0300

    SHIEEEEEEEEEET

---
+++ cmake-build-debug/CMakeFiles/fdf_1.dir/DependInfo.cmake
@@ -0,0 +1,97 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_atoi.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_atoi.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_bzero.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_bzero.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_countwords.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_countwords.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_isalnum.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_isalnum.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_isalpha.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_isalpha.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_isascii.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_isascii.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_isdigit.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_isdigit.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_isprint.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_isprint.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_isspace.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_isspace.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_itoa.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_itoa.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_memalloc.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_memalloc.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_memccpy.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_memccpy.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_memchr.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_memchr.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_memcmp.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_memcmp.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_memcpy.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_memcpy.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_memdel.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_memdel.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_memmove.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_memmove.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_memset.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_memset.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_putchar.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_putchar.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_putchar_fd.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_putchar_fd.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_putendl.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_putendl.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_putendl_fd.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_putendl_fd.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_putnbr.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_putnbr.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_putnbr_fd.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_putnbr_fd.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_putstr.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_putstr.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_putstr_fd.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_putstr_fd.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strcat.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strcat.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strchr.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strchr.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strclr.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strclr.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strcmp.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strcmp.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strcpy.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strcpy.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strdel.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strdel.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strdup.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strdup.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strequ.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strequ.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_striter.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_striter.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_striteri.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_striteri.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strjoin.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strjoin.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strlcat.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strlcat.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strlen.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strlen.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strmap.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strmap.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strmapi.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strmapi.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strncat.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strncat.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strncmp.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strncmp.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strncpy.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strncpy.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strnequ.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strnequ.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strnew.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strnew.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strnstr.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strnstr.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strrchr.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strrchr.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strsplit.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strsplit.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strstr.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strstr.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strsub.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strsub.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_strtrim.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_strtrim.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_tolower.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_tolower.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/ft_toupper.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/ft_toupper.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/libft/get_next_line.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/libft/get_next_line.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/minilibx_macos/font.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/minilibx_macos/font.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/minilibx_macos/mlx_init_loop.m" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/minilibx_macos/mlx_init_loop.m.o"
+  "/Users/tharle/CLionProjects/fdf-1/minilibx_macos/mlx_int_str_to_wordtab.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/minilibx_macos/mlx_int_str_to_wordtab.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/minilibx_macos/mlx_new_image.m" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/minilibx_macos/mlx_new_image.m.o"
+  "/Users/tharle/CLionProjects/fdf-1/minilibx_macos/mlx_new_window.m" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/minilibx_macos/mlx_new_window.m.o"
+  "/Users/tharle/CLionProjects/fdf-1/minilibx_macos/mlx_rgb.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/minilibx_macos/mlx_rgb.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/minilibx_macos/mlx_shaders.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/minilibx_macos/mlx_shaders.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/minilibx_macos/mlx_xpm.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/minilibx_macos/mlx_xpm.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/alter.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/alter.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/colorer.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/colorer.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/drawer.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/drawer.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/extra_1.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/extra_1.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/extra_2.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/extra_2.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/initializer.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/initializer.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/keys_n_buttons.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/keys_n_buttons.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/main.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/main.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/mouse_handler.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/mouse_handler.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/projector.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/projector.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/reader.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/reader.c.o"
+  "/Users/tharle/CLionProjects/fdf-1/srcs/stack_handler.c" "/Users/tharle/CLionProjects/fdf-1/cmake-build-debug/CMakeFiles/fdf_1.dir/srcs/stack_handler.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../includes"
+  "../libft/includes"
+  "../minilibx_macos"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
